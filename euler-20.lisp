979fcc3a87e1e7e051c6970c5b7cbd86c37f1192
Christopher Wellons <mosquitopsu@gmail.com>
2012-10-14 20:52:22 -0400

    Add problem 20.

---
+++ euler-20.lisp
@@ -0,0 +1,13 @@
+;;; Project Euler - Problem 20
+
+;; n! means n × (n − 1) × ... × 3 × 2 × 1
+
+;; For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800, and the sum
+;; of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+;; Find the sum of the digits in the number 100!
+
+(defun euler-20 ()
+  (loop with n = (apply #'* (loop for i from 2 to 100 collect i))
+     until (zerop n) sum (mod n 10)
+     do (setf n (floor n 10))))
