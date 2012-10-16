1d609d4155b6e8cbd77e8c5c49e3eea1b53f82f3
Christopher Wellons <mosquitopsu@gmail.com>
2012-10-15 20:32:37 -0400

    Load with ASDF.

--- benchmark.lisp
+++ benchmark.lisp
@@ -3,9 +3,9 @@
 ;; It's interesting to see the differences (speed, warnings, errors)
 ;; between the various Common Lisp implementations.
 
+(asdf:load-system :euler-cl)
+
 (time
  (loop for problem from 1 to 25
-    initially (load "common.lisp")
-    do (load (format nil "euler-~d.lisp" problem))
     do (format t "Problem ~3,d: ~d~%" problem
                (funcall (intern (format nil "EULER-~d" problem))))))
