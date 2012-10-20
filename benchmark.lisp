82000252703070274e09bd13f9effdca68705404
Christopher Wellons <mosquitopsu@gmail.com>
2012-10-19 21:20:42 -0400

    Improve benchmarking.

--- benchmark.lisp
+++ benchmark.lisp
@@ -5,7 +5,14 @@
 
 (asdf:load-system :euler-cl)
 
+(defun run-problem (n)
+  "Run Project Euler problem N returning the answer and the run time."
+  (let ((start (get-internal-real-time)))
+    (values (funcall (intern (format nil "EULER-~d" n)))
+            (/ (- (get-internal-real-time) start) 1.0
+               internal-time-units-per-second))))
+
 (time
- (loop for problem from 1 to 31
-    do (format t "Problem ~3,d: ~d~%" problem
-               (funcall (intern (format nil "EULER-~d" problem))))))
+ (loop for problem from 1 to 31 do
+      (format t "Problem ~3,d: ~{~d ~30t(~6,3f seconds)~}~%" problem
+              (multiple-value-list (run-problem problem)))))
